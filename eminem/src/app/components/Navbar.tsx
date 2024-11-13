bceb12c86bd373e85e4900ab46106fe726fc20cf
Patryk Kurek <patryk744@op.pl>
2024-11-13 23:54:13 +0100

    feat: crud frontend

--- eminem/src/app/components/Navbar.tsx
+++ eminem/src/app/components/Navbar.tsx
@@ -2,16 +2,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4 text-white flex justify-between items-center">
-      <Link href="/" className="flex items-center space-x-4">
-        <img src="/favicon.ico" alt="UpMovie Logo" className="w-8 h-8" />
-        <span className="text-2xl font-bold">UpMovie</span>
-      </Link>
-      <div className="flex space-x-6">
-     
-        <Link href="/auth">
-          <p className="hover:text-gray-300">Sign Up/Sign In</p>
-        </Link>
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold">Home</Link>
+        <div className="space-x-4">
+          <Link href="/movies" className="hover:text-gray-300">Manage Movies</Link>
+          <Link href="/auth">
+            <p className="hover:text-gray-300">Sign Up/Sign In</p>
+          </Link>
+        </div>
       </div>
     </nav>
   );
